cd3ded82ff3a445f55c65dd055a29a1660439c51
nsouch <nsouch@users.noreply.github.com>
2025-12-30 10:21:35 +0100

    sbt.sh openjdk11

--- payola/sbt.sh
+++ payola/sbt.sh
@@ -1,3 +1,7 @@
-export JAVA_HOME='/usr/lib/jvm/jdk-7-oracle-x64'
+# export JAVA_HOME=/usr/local/opt/openjdk
+export JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.26
+# export JAVA_HOME=/Users/nico/Downloads/zulu8.90.0.19-ca-jdk8.0.472-macosx_x64
+# export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-8.jdk/Contents/Home/
 
-$JAVA_HOME/jre/bin/java -Xmx1024M -XX:MaxPermSize=512M -Xss2M -jar `dirname $0`/sbt-launch.jar "$@"
+#$JAVA_HOME/bin/java -Xmx1024M -XX:MaxPermSize=512M -Xss2M -jar `dirname $0`/sbt-launch.jar "$@"
+$JAVA_HOME/bin/java -Xmx1024M -Xss2M -jar `dirname $0`/sbt-launch.jar "$@"
