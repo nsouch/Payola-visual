6393bcbe656009cc4a941e7a02c45ad9cb786185
nsouch <nsouch@users.noreply.github.com>
2026-01-11 09:17:11 +0100

    buildClasspath

--- payola/s2js/compiler/src/main/scala/s2js/compiler/ScalaToJsCompiler.scala
+++ payola/s2js/compiler/src/main/scala/s2js/compiler/ScalaToJsCompiler.scala
@@ -1,6 +1,7 @@
 package s2js.compiler
 
 import tools.nsc.{Global, Settings}
+import scala.tools.nsc.io.Directory
 
 /**
   * A Scala to JavaScript compiler.
@@ -16,9 +17,42 @@ class ScalaToJsCompiler(
         "createPackageStructure:" + createPackageStructure.toString
     )
 
+    private def buildClasspath(): String = {
+        // 1. Get Scala standard libraries (scala-library, scala-reflect, scala-compiler)
+        val scalaLibraries = Seq(
+            classOf[List[_]],           // scala-library
+            classOf[scala.reflect.api.TypeCreator], // scala-reflect
+            classOf[Global]              // scala-compiler
+        ).map { clazz =>
+            val location = clazz.getProtectionDomain.getCodeSource.getLocation
+            new java.io.File(location.toURI).getAbsolutePath
+        }
+        
+        // 2. Get current runtime classpath
+        val runtimeClasspath = System.getProperty("java.class.path")
+            .split(java.io.File.pathSeparator)
+            .toSeq
+        
+        // 3. Combine provided classpath with Scala libraries and runtime classpath
+        val providedClasspath = if (classPath != null && classPath.nonEmpty) {
+            classPath.split(java.io.File.pathSeparator).toSeq
+        } else {
+            Seq.empty
+        }
+        
+        // 4. Combine all classpaths and remove duplicates
+        (providedClasspath ++ scalaLibraries ++ runtimeClasspath)
+            .distinct
+            .mkString(java.io.File.pathSeparator)
+    }
+
     private val settings = new Settings()
-    settings.classpath.value = classPath
+    settings.classpath.value = buildClasspath()
     settings.outdir.value = targetDirectory
+    // Disable warnings for better compatibility
+    settings.nowarn.value = true
+    settings.deprecation.value = false
+    settings.feature.value = false
 
     /**
       * Compiles the specified Scala source files into JavaScript.
